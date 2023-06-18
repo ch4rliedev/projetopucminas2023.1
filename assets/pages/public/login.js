a0c9d769ef776d692fa552f35cb4dc198418d3ee
Charlie <88893508+ch4rliedev@users.noreply.github.com>
2023-06-18 14:36:27 -0300

    Add files via upload
    Adicionado verificações adicionais de login

--- assets/pages/public/login.js
+++ assets/pages/public/login.js
@@ -3,31 +3,34 @@ let listCadastro = JSON.parse(localStorage.getItem("listCadastro"));
 function entrar(tipo) {
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
+  let checkUser = false;
 
-  for (var i = 0; i < listCadastro.length; i++) {
-    console.log("oi");
-    if (
-      email.trim() == listCadastro[i].email &&
-      senha.trim() == listCadastro[i].senha
-    ) {
+  for (let i = 0; i < listCadastro.length; i++) {
+    if (email.trim() == listCadastro[i].email && senha.trim() == listCadastro[i].senha) {
       if (listCadastro[i].tipo == tipo) {
-        // alert("Login efetuado com sucesso");
+        alert("Login efetuado com sucesso");
+        localStorage.setItem("userCurrent", email);
+        checkUser = true
         if (tipo == 1) {
           window.location.href = "./../influencer/portal_influencer.html";
         } else {
           window.location.href = "./../enterprise/portal_empresa.html";
         }
         break;
-      } else {
+      } 
+      else {
         if (tipo == 1) {
-          // alert("Você não esta cadastrado como influencer");
+          alert("Você não está cadastrado como influencer, logue como empresa.");
+          return;
         } else {
-          // alert("Você não esta cadastrado como empresa");
+          alert("Você não está cadastrado como empresa, logue como influencer.");
+          return;
         }
       }
-    } else {
-      erro = true;
-      // alert("email ou senha incorretos!");
     }
+  }
+
+  if (!checkUser) {
+    alert("E-mail ou senha incorretos!");
   }
 }
